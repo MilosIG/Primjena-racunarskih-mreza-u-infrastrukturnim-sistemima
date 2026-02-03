e7cb11a4f038da154381483eb0947543c0bf3ec1
MilosIG <ignjaticm104@gmail.com>
2026-02-03 18:25:48 +0100

    Kreirana klasa GameManager radi implementacije funkcionalnih cjelina.

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5abf29ee57469424acfbd5d34c854c72bd465086")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c5d331166679cb7d5964c5bec1805649bb7b4b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
